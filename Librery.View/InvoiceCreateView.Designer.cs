324ab27b09c76136cdaa1ec168fb90f90095090b
Oscar Eliseo Torres Vasquez <nombrede79@gmail.com>
2024-11-11 08:02:51 -0600

    add buttons

--- Librery.View/InvoiceCreateView.Designer.cs
+++ Librery.View/InvoiceCreateView.Designer.cs
@@ -161,7 +161,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button1.Location = new System.Drawing.Point(316, 384);
+            this.button1.Location = new System.Drawing.Point(355, 384);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(86, 61);
             this.button1.TabIndex = 11;
@@ -175,7 +175,7 @@
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button2.Location = new System.Drawing.Point(424, 384);
+            this.button2.Location = new System.Drawing.Point(463, 384);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(86, 61);
             this.button2.TabIndex = 12;
