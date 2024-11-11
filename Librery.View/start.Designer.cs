1823b6b7a0221d1e5cd87cd840024d68edf631a5
Oscar Eliseo Torres Vasquez <nombrede79@gmail.com>
2024-11-11 08:11:54 -0600

    add text

--- Librery.View/start.Designer.cs
+++ Librery.View/start.Designer.cs
@@ -109,10 +109,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(177, 13);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, -2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 31);
+            this.label1.Size = new System.Drawing.Size(135, 55);
             this.label1.TabIndex = 6;
             this.label1.Text = "Inicio";
             // 
