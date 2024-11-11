4f7f864c58c739d593fcba226feca981d43fee62
Oscar Eliseo Torres Vasquez <nombrede79@gmail.com>
2024-11-11 08:01:12 -0600

    add texts

--- Librery.View/InvoiceCreateView.Designer.cs
+++ Librery.View/InvoiceCreateView.Designer.cs
@@ -56,10 +56,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Sporting Grotesque", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(308, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(373, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(267, 47);
+            this.label1.Size = new System.Drawing.Size(196, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nueva factura";
             this.label1.Click += new System.EventHandler(this.label1_Click);
